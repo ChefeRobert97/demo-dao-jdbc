f63607cb19323e49e8d436ff97e098abae11a37e
Robert Fernandes <robertkirka97@gmail.com>
2024-09-09 16:46:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,21 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 		Department d = new Department(1, "Books");
-		System.out.println(d);
+		Date date = sdf.parse("07/04/1997 16:43:01");
+		
+		Seller s = new Seller(1, "Robert", "Robert@gmail.com", date, 3000.0, d);
+		System.out.println(s);
 		
 	}
 
