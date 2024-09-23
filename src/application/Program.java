c8d9ee8b4cf4675a457d66e573c0473ca8313408
Robert Fernandes <robertkirka97@gmail.com>
2024-09-23 12:47:05 -0300

    Reusing Instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,8 +16,9 @@ public class Program {
 //		Seller s = new Seller(1, "Robert", "Robert@gmail.com", date, 3000.0, d);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		System.out.println("=== TEST 1: SELLER FINDBYID ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 	}
