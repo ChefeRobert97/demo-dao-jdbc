02798cdd7d45ca640bb7c2729846512c58fcbc7f
Robert Fernandes <robertkirka97@gmail.com>
2024-11-26 15:26:45 -0300

    DepartmentDaoJDBC and update DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		
 //		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 //		Department dep = new Department(1, "Books");
 //		System.out.println(dep);
@@ -48,6 +52,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 6: Seller Delete ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+
+		sc.close();
 	}
 
 }
