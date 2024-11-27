cc84d8cd2a47453865391e9df57e4e8935b8ac0e
Robert Fernandes <robertkirka97@gmail.com>
2024-11-27 14:30:10 -0300

    delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -25,16 +25,23 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: Insert =======");
-		Department newDepartment = new Department(null, "Music");
-		departmentDao.insert(newDepartment);
-		System.out.println("Inserted! New id: " + newDepartment.getId());
+		/*
+		 * System.out.println("\n=== TEST 3: Insert ======="); Department newDepartment
+		 * = new Department(null, "Music"); departmentDao.insert(newDepartment);
+		 * System.out.println("Inserted! New id: " + newDepartment.getId());
+		 */		
+//		System.out.println("\n=== TEST 4: update =======");
+//		Department dep2 = departmentDao.findById(1);
+//		dep2.setName("Food");
+//		departmentDao.update(dep2);
+//		System.out.println("Update Completed");
 		
-		System.out.println("\n=== TEST 4: update =======");
-		Department dep2 = departmentDao.findById(1);
-		dep2.setName("Food");
-		departmentDao.update(dep2);
-		System.out.println("Update Completed");
+		System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
 		
 		sc.close();
 	}
